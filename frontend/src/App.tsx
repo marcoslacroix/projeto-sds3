363d52064a0a8b0e6f53c4e85e8cc2a286e4efbe
marcoslacroix <marcoslacroix@gmail.com>
2021-09-07 20:53:12 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-        <h1>Olá Mundo</h1>
+    <>
+    <div className="container">
+        <NavBar />
+        <h1 className="text-primary">Olá Mundo</h1>
+        <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
